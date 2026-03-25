b97cb7d616297cf2089def0a4ec4aaf6cd3d37a0
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-03-25 17:28:49 +0200

    Added needed elements as const. Added inline styles for paragraphs and changed fontFamily for body tag

---
+++ Task01/script.js
@@ -0,0 +1,24 @@
+const bodyTag = document.querySelector('body');
+const p1 = document.querySelector('.p1');
+const p2 = document.querySelector('.p2');
+const p3 = document.querySelector('.p3');
+
+p1.style.color = 'orange';
+p1.style.background = 'yellow';
+p1.style.fontSize = '16px';
+p1.style.padding = '5px';
+p1.style.textAlign = 'center';
+
+p2.style.color = 'blue';
+p2.style.background = 'skyblue';
+p2.style.fontSize = '14px';
+p2.style.padding = '5px';
+p2.style.textAlign = 'right';
+
+p3.style.color = 'red';
+p3.style.background = 'palevioletred';
+p3.style.fontSize = '12px';
+p3.style.padding = '5px';
+p3.style.textAlign = 'left';
+
+bodyTag.style.fontFamily = "'Arial', 'Helvetica', 'Arial', sans-serif";``
