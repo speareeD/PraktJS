827e9f4f6672e2f4193a727e2becff7ec4df8c26
VladyslavSliptsov <ipz254_svv@student.ztu.edu.ua>
2026-03-26 17:33:04 +0200

    Used for loop to change background of odd children of tableTag

--- Task01/script.js
+++ Task01/script.js
@@ -2,6 +2,7 @@ const bodyTag = document.querySelector('body');
 const p1 = document.querySelector('.p1');
 const p2 = document.querySelector('.p2');
 const p3 = document.querySelector('.p3');
+const tableTag = document.querySelector('.table');
 
 p1.style.color = 'orange';
 p1.style.background = 'yellow';
@@ -22,3 +23,9 @@ p3.style.padding = '5px';
 p3.style.textAlign = 'left';
 
 bodyTag.style.fontFamily = "'Arial', 'Helvetica', 'Arial', sans-serif";``
+
+for (let i = 0; i < 25; i++) {
+    if(i % 2 === 0) {
+        tableTag.children[i].style.background = 'yellow';
+    }
+}
